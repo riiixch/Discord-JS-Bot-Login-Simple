8a8010ff222eb781e36734bd661584a71096e9bb
RIIIXCH <bigowne.server.netmail@gmail.com>
2023-12-13 22:59:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
-const { token } = require('./config.json');
+const { token } = require('./config.js');
 
 function log(x) {
     return console.log(x)
